5656dd3420abfd3834391645946286d30f5ef570
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-07-12 04:42:06 -0300

    add some dummy funcs

--- app/src/main/java/unioeste/br/openvrt/MainActivity.java
+++ app/src/main/java/unioeste/br/openvrt/MainActivity.java
@@ -1,8 +1,9 @@
 package unioeste.br.openvrt;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,19 +12,45 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Integer selectedMeasurement = 0;
+
+    private AlertDialog.Builder measurementDialogBuilder = null;
+
+    private void createMeasurementDialog() {
+        measurementDialogBuilder = new AlertDialog.Builder(this);
+        measurementDialogBuilder.setTitle("yo");
+        measurementDialogBuilder.setSingleChoiceItems(R.array.measurements, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                selectedMeasurement = i;
+            }
+        });
+        measurementDialogBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                //
+            }
+        });
+        measurementDialogBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                //
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        createMeasurementDialog();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                measurementDialogBuilder.create();
             }
         });
     }
