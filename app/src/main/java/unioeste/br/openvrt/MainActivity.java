b1cc2301f15707a71c70041e1030b80cddc56067
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-07-12 21:49:36 -0300

    spinner

--- app/src/main/java/unioeste/br/openvrt/MainActivity.java
+++ app/src/main/java/unioeste/br/openvrt/MainActivity.java
@@ -1,77 +1,44 @@
 package unioeste.br.openvrt;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private Integer selectedMeasurement = 0;
 
-    private AlertDialog measurementDialog = null;
-
-    private void createMeasurementDialog() {
+    private AlertDialog createMeasurementDialog() {
         AlertDialog.Builder measurementDialogBuilder = new AlertDialog.Builder(this);
-        measurementDialogBuilder.setTitle("yo");
-        measurementDialogBuilder.setSingleChoiceItems(R.array.measurements, 0, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                selectedMeasurement = i;
-            }
+        measurementDialogBuilder.setTitle(R.string.rate_measurement);
+        measurementDialogBuilder.setSingleChoiceItems(R.array.measurements, 0, (dialogInterface, i) -> selectedMeasurement = i);
+        measurementDialogBuilder.setPositiveButton(R.string.ok, (dialog, id) -> {
+            //
         });
-        measurementDialogBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                //
-            }
+        measurementDialogBuilder.setNegativeButton(R.string.cancel, (dialog, id
+        ) -> {
+            //
         });
-        measurementDialogBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                //
-            }
+        return measurementDialogBuilder.create();
+    }
+
+    private AlertDialog createSpinnerDialog() {
+        AlertDialog.Builder spinnerDialogBuilder = new AlertDialog.Builder(this);
+        spinnerDialogBuilder.setTitle(R.string.scanning_maps);
+        spinnerDialogBuilder.setView(R.layout.progress_dialog);
+        spinnerDialogBuilder.setNegativeButton(R.string.cancel, (dialog, id) -> {
+            //
         });
-        measurementDialog = measurementDialogBuilder.create();
+        return spinnerDialogBuilder.create();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        createMeasurementDialog();
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                measurementDialog.show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> createSpinnerDialog().show());
     }
 }
