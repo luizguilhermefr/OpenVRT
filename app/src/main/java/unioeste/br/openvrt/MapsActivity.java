85a4a9dd3da9986f252f5b0d6a12fabb62cee7eb
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-08-02 01:54:13 -0300

    track user movement

--- app/src/main/java/unioeste/br/openvrt/MapsActivity.java
+++ app/src/main/java/unioeste/br/openvrt/MapsActivity.java
@@ -2,22 +2,27 @@ package unioeste.br.openvrt;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.data.geojson.GeoJsonLayer;
 import org.json.JSONException;
 import org.json.JSONObject;
 import unioeste.br.openvrt.file.PrescriptionMapReader;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.Objects;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap googleMap;
 
@@ -25,7 +30,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private String mapLocation;
 
+    private static final long MIN_TIME = 400;
+
     private static final int PERMISSION_ACCESS_FINE_LOCATION = 2;
+    private static final float MIN_DISTANCE = 1000;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,20 +97,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @SuppressLint("MissingPermission")
     private void useUserLocation() {
-        runOnUiThread(() -> googleMap.setMyLocationEnabled(true));
+        runOnUiThread(() -> {
+            googleMap.setMyLocationEnabled(true);
+            googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Objects.requireNonNull(locationManager).requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+        });
     }
 
     private void addMapLayerToMap(JSONObject geoJson) {
         mapLayer = new GeoJsonLayer(googleMap, geoJson);
-        runOnUiThread(() -> {
-            mapLayer.addLayerToMap();
-        });
+        runOnUiThread(() -> mapLayer.addLayerToMap());
         askPermissionsToUseGpsOrCenterMap();
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
+        this.googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         openFile();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 16);
+        this.googleMap.animateCamera(cameraUpdate);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
